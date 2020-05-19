3ae070a532d5a4f54cec5989f0c860fdffe545e1
tangtanglove <l>
2020-05-19 16:02:06 +0800

    修正代码

--- app/Http/Controllers/Auth/WxMpLoginController.php
+++ app/Http/Controllers/Auth/WxMpLoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+use App\User;
 use App\Models\ActionLog;
 use EasyWeChat\Factory;
 use Str;
