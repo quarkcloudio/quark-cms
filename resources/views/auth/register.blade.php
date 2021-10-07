ef8f7c30df1ffbebc7b3a4cb98ba916f5cf180d6
tangtanglove <dai_hang_love@126.com>
2021-10-07 16:17:48 +0800

    fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="margin:60px 0px 100px 0px">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
