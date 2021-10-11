8fe57b6eb9fd4129f374e411aa18e911351d56d8
tangtanglove <dai_hang_love@126.com>
2021-10-11 09:39:26 +0800

    fix

--- modules/Wechat/Database/Seeders/MenusTableSeeder.php
+++ modules/Wechat/Database/Seeders/MenusTableSeeder.php
@@ -14,12 +14,13 @@ class MenusTableSeeder extends Seeder
      */
     public function run()
     {
+        $getId = DB::table('menus')->insertGetId(['name' => '微信管理','guard_name' => 'admin','icon' => 'icon-shop','type'=>'default','pid' => '0','sort' => '-1','path' => '/wechat','show'  => '1','status' => '1']);
+
         DB::table('menus')->insert([
-            ['id' =>'64','name' => '微信管理','guard_name' => 'admin','icon' => 'icon-shop','type'=>'default','pid' => '0','sort' => '-1','path' => '/wechat','show'  => '1','status' => '1'],
-            ['id' =>'65','name' => '公众号配置','guard_name' => 'admin','icon' => '','type'=>'engine','pid' => '64','sort' => '0','path' => 'admin/wechatConfig/setting-form','show'  => '1','status' => '1'],
-            ['id' =>'67','name' => '同步用户','guard_name' => 'admin','icon' => '','type'=>'engine','pid' => '64','sort' => '0','path' => 'admin/wechatSyncUserTask/index','show'  => '1','status' => '1'],
-            ['id' =>'68','name' => '订阅号菜单','guard_name' => 'admin','icon' => '','type'=>'engine','pid' => '64','sort' => '0','path' => 'admin/dyhMenu/index','show'  => '1','status' => '1'],
-            ['id' =>'69','name' => '服务号菜单','guard_name' => 'admin','icon' => '','type'=>'engine','pid' => '64','sort' => '0','path' => 'admin/fwhMenu/index','show'  => '1','status' => '1'],
+            ['name' => '公众号配置','guard_name' => 'admin','icon' => '','type'=>'engine','pid' => $getId,'sort' => '0','path' => 'admin/wechatConfig/setting-form','show'  => '1','status' => '1'],
+            ['name' => '同步用户','guard_name' => 'admin','icon' => '','type'=>'engine','pid' => $getId,'sort' => '0','path' => 'admin/wechatSyncUserTask/index','show'  => '1','status' => '1'],
+            ['name' => '订阅号菜单','guard_name' => 'admin','icon' => '','type'=>'engine','pid' => $getId,'sort' => '0','path' => 'admin/dyhMenu/index','show'  => '1','status' => '1'],
+            ['name' => '服务号菜单','guard_name' => 'admin','icon' => '','type'=>'engine','pid' => $getId,'sort' => '0','path' => 'admin/fwhMenu/index','show'  => '1','status' => '1'],
         ]);
     }
 }
